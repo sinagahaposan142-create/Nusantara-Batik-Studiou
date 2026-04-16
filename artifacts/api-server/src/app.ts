708b78f0010cb2d2e8940ddfffbfd8b57507c52f
sinagahaposan142-create <sinagahaposan142@gmail.com>
2026-04-16 15:36:58 +0000

    fix pino final stable

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -4,20 +4,22 @@ import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
+const pino = pinoHttp as unknown as any;
+
 const app: Express = express();
 
 app.use(
-  pinoHttp({
+  pino({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
